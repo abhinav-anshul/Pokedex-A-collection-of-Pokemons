8190d5caa28a92a1193365d7fc64144cb14a37c3
abhinav anshul <abhinavanshul4u@gmail.com>
2019-10-19 22:46:47 +0530

    not found page added

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Page not found</p>
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
--- src/components/PokemonRouter.js
+++ src/components/PokemonRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import PokemonDetails from "./PokemonDetails";
+import NotFoundPage from "./NotFoundPage";
 
 const Route = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Dashboard} />
       <Route path="/pokemon" component={PokemonDetails} />
+      <Route component={NotFoundPage} />
     </Switch>
   </BrowserRouter>
 );
