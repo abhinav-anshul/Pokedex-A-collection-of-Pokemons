c26feac58495fdb6adcf0fc38a910041a9347910
abhinav anshul <abhinavanshul4u@gmail.com>
2019-10-20 21:25:53 +0530

    pokemonDetails page added and routed

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,29 +1,190 @@
 import React from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class PokemonDetails extends React.Component {
   state = {
     name: "",
     pokemonIndex: "",
-    imageUrl: ""
+    imageUrl: "",
+    types: [],
+    description: "",
+    stats: {
+      hp: "",
+      attack: "",
+      defense: "",
+      speed: "",
+      specialAttack: "",
+      specialDefense: ""
+    },
+
+    height: "",
+    weight: "",
+    eggGroups: "",
+    abilities: "",
+    genderRatioMale: "",
+    genderRatioFemale: "",
+    evs: "",
+    hatchSteps: ""
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     const { pokemonIndex } = this.props.match.params;
 
     const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
-    const pokemonSpecies = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}`;
+    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}`;
 
-    console.log(pokemonIndex);
-    console.log(pokemonUrl);
-    console.log(pokemonSpecies);
+    // console.log(pokemonIndex);
+    // console.log(pokemonUrl);
+    // console.log(pokemonSpecies);
+
+    const pokemonRes = await axios.get(pokemonUrl);
+
+    const name = pokemonRes.data.name;
+    const imageUrl = pokemonRes.data.sprites.front_default;
+
+    let { hp, attack, defense, speed, specialAttack, specialDefense } = "";
+
+    pokemonRes.data.stats.map(stat => {
+      switch (stat.stat.name) {
+        case "hp":
+          hp = stat["base_stat"];
+          break;
+        case "attack":
+          attack = stat["base_stat"];
+          break;
+        case "defense":
+          defense = stat["base_stat"];
+          break;
+        case "speed":
+          speed = stat["base_stat"];
+          break;
+        case "special-attack":
+          specialAttack = stat["base_stat"];
+          break;
+        case "special-defense":
+          specialDefense = stat["base_stat"];
+          break;
+      }
+    });
+
+    const height =
+      Math.round((pokemonRes.data.height * 0.328084 + 0.0001) * 100) / 100;
+
+    const weight = Math.round(pokemonRes.data.weight / 10);
+
+    const types = pokemonRes.data.types.map(type => type.type.name);
+
+    const abilities = pokemonRes.data.abilities.map(ability => {
+      return ability.ability.name
+        .toLowerCase()
+        .split("-")
+        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
+    });
+
+    const evs = pokemonRes.data.stats
+      .filter(stat => {
+        if (stat.effort > 0) {
+          return true;
+        }
+        return false;
+      })
+      .map(stat => {
+        return `${stat.effort} ${stat.stat.name}`
+          .toLowerCase()
+          .split("-")
+          .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(" ");
+      })
+      .join(", ");
+
+    //Get pokemon description,catch rate, egggroups, gender ratio, hatch steps
+    await axios.get(pokemonSpeciesUrl).then(res => {
+      let description = "";
+      res.data.flavor_text_entries.some(flavor => {
+        if (flavor.language.name === "en") {
+          description = flavor.flavor_text;
+          return;
+        }
+      });
+
+      const femaleRate = res.data["gender_rate"];
+      const genderRatioFemale = 12.5 * femaleRate;
+      const genderRatioMale = 12.5 * (8 - femaleRate);
+
+      const catchRate = Math.round(100 / 255) * res.data["capture_rate"];
+
+      const eggGroups = res.data["egg_groups"]
+        .map(group => {
+          return group.name
+            .toLowerCase()
+            .split("-")
+            .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(" ");
+        })
+        .join(", ");
+
+      const hatchSteps = 255 * (res.data["hatch_counter"] + 1);
+
+      this.setState({
+        description,
+        genderRatioFemale,
+        genderRatioMale,
+        catchRate,
+        eggGroups,
+        hatchSteps
+      });
+    });
+
+    this.setState({
+      imageUrl,
+      pokemonIndex,
+      name,
+      types,
+      stats: {
+        hp,
+        attack,
+        defense,
+        speed,
+        specialAttack,
+        specialDefense
+      },
+      height,
+      weight,
+      abilities,
+      evs
+    });
   }
 
   render() {
     return (
       <div>
         <Navbar />
+        <div className="col">
+          <div className="card">
+            <div className="card-header">
+              <div className="row">
+                <div className="col-5">
+                  <h5>{this.state.pokemonIndex}</h5>
+                </div>
+                <div className="col-7">
+                  <div className="float-right">
+                    {this.state.types.map(type => (
+                      <span
+                        key={type}
+                        className="badge badge-primary badge-pill mr-1"
+                      >
+                        {type}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
